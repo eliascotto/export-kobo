1a8d2767be60ac3ba67a4e3d06ffe3046b09eef8
cjelly <cjelly@TheJelly.localdomain>
2022-10-09 13:40:17 -0400

    Cleaned up formatting of commit: b1762e3 (Added check if item's self.text was None, 2022-10-09))

--- export-kobo.py
+++ export-kobo.py
@@ -126,10 +126,7 @@ class Item(object):
 
     def __init__(self, values):
         self.volumeid = values[0]
-        if values[1] is not None:
-            self.text = values[1].strip().rstrip()
-        else:
-            self.text = None
+        self.text = values[1].strip().rstrip() if values[1] is not None else None
         self.annotation = values[2]
         self.extraannotationdata = values[3]
         self.datecreated = values[4] if values[4] is not None else "1970-01-01T00:00:00.000"
